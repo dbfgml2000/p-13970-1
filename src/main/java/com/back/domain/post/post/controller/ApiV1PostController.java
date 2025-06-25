0e8cfc3aca21bda7235efacb9f9dc7e1755d6f7a
dbfgml20 <dbfgml20@gmail.com>
2025-06-25 15:27:59 +0900

    11강-댓글 삭제 api 구현(GET 방식)

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -4,9 +4,8 @@ import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +16,7 @@ public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
+    @Transactional(readOnly = true)
     public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
 
@@ -26,7 +26,8 @@ public class ApiV1PostController {
                 .toList();
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public PostDto getItem(int id) {
         Post post = postService.findById(id).get();
 
