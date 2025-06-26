3dad0b39a0bd435c9e256fca5d17e732826f895d
dbfgml20 <dbfgml20@gmail.com>
2025-06-26 14:51:41 +0900

    31강 - PostComment 수정 api 추가

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,6 +5,9 @@ import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -84,6 +87,30 @@ public class ApiV1PostCommentController {
                 new PostCommentDto(post.getComments().get(post.getComments().size() - 1)) // 가장 최근 댓글 반환
         );
     }
+
      */
 
+    record PostCommentModifyReqBody(
+            @NotBlank @Size(min=2, max=100)
+            String content
+    ) {}
+
+    @PutMapping("/{id}")
+    @Transactional
+    public RsData<PostCommentDto> modify(
+            @PathVariable int postId,
+            @PathVariable int id,
+            @Valid @RequestBody PostCommentModifyReqBody reqBody
+    ) {
+        Post post = postService.findById(postId).get();
+
+        PostComment postComment = post.findCommentById(id).get();
+
+        postService.modifyComment(postComment, reqBody.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 댓글이 수정되었습니다.".formatted(id)
+        );
+    }
 }
