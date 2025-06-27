bb4b46a02a9c84fa6c35372031a650881d0dac43
dbfgml20 <dbfgml20@gmail.com>
2025-06-27 14:52:39 +0900

    48강 - 전역 예외 핸들러 추가

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -1,22 +1,36 @@
 package com.back.global.globalExceptionHandler;
 
-
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
-@RestControllerAdvice
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND) // 응답 헤더의 코드(404 Not Found)
-    public ResponseEntity<RsData<Void>> handle(NoSuchElementException exception) {
+    public ResponseEntity<RsData<Void>> handle() {
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "404-1",
+                        "해당 데이터가 존재하지 않습니다."
+                ),
+                NOT_FOUND
+        );
+    }
+}
+
+/*
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<RsData<Void>> handle() {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND) // 응답 헤더의 코드(404 Not Found)
                 .body( // 응답 바디
@@ -27,6 +41,8 @@ public class GlobalExceptionHandler {
                 );
     }
 }
+
+ */
 
 /*
 import com.back.global.rsData.RsData;
