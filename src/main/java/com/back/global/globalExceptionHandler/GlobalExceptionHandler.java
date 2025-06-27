58f27aa8abfd279b9bd2b7daa174ddd3313c44d1
dbfgml20 <dbfgml20@gmail.com>
2025-06-27 14:52:56 +0900

    48강 - 전역 예외 핸들러 추가

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -45,17 +45,6 @@ public class GlobalExceptionHandler {
  */
 
 /*
-import com.back.global.rsData.RsData;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.NoSuchElementException;
-
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
